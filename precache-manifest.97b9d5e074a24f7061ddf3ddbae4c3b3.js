208808976f96845bf609b9b9e6ff410bd6f7d1c4
Joseph Ostrow <jstro@umich.edu>
2019-09-15 22:47:32 -0400

    Updates

---
+++ precache-manifest.97b9d5e074a24f7061ddf3ddbae4c3b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46780236759f38ae683be1d4dcff2abe",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "d824ec2c9a27e67bb636",
+    "url": "/youtube-clone/static/css/main.13be74f8.chunk.css"
+  },
+  {
+    "revision": "079393fa9fad7364d518",
+    "url": "/youtube-clone/static/js/2.cc532e35.chunk.js"
+  },
+  {
+    "revision": "d824ec2c9a27e67bb636",
+    "url": "/youtube-clone/static/js/main.8b6078f2.chunk.js"
+  },
+  {
+    "revision": "a15bf1e9bf9a5f14bed6",
+    "url": "/youtube-clone/static/js/runtime~main.ee0b5924.js"
+  }
+]);
